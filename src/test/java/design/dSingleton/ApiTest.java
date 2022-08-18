2c8ffbcd74474b89f05ee1dffa8747c8546e69e9
songshenghua <songshenghua@oceanus-plus.com>
2022-08-18 14:56:21 +0800

    原型模式

--- src/test/java/design/dSingleton/ApiTest.java
+++ src/test/java/design/dSingleton/ApiTest.java
@@ -1,14 +1,17 @@
 package design.dSingleton;
 
 import com.demo.design.dSingleton.Singleton;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
+@Slf4j
 public class ApiTest {
 
     @Test
     public void test_dSingleton() {
 
-        Singleton singleton = Singleton.INSTANCE;
+        log.info(String.valueOf(Singleton.INSTANCE.hashCode()));
+        log.info(String.valueOf(Singleton.INSTANCE.hashCode()));
 
     }
 }
